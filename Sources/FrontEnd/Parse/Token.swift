3baa7769e35f741acf0997229fd90f73cda4f071
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-05-31 14:28:13 +0530

    feat: added token

---
+++ Sources/FrontEnd/Parse/Token.swift
@@ -0,0 +1,71 @@
+public struct Token {
+  public enum Kind: Int {
+    // Errors
+    case Invalid = 0
+    case unterminatedString
+    case unterminatedBlockComment
+
+    // Keywords
+    case `and` = 1000
+    case `class`
+    case `else`
+    case `false`
+    case `fun`
+    case `for`
+    case `if`
+    case `nil`
+    case `not`
+    case `or`
+    case `print`
+    case `return`
+    case `super`
+    case `this`
+    case `true`
+    case `var`
+    case `while`
+
+    // Literals
+    case identifier = 2000
+    case string
+    case number
+
+    // Operators
+    case `not_equals` = 3000
+    case `equals`
+    case `greater_equals`
+    case `less_equals`
+    case `greater`
+    case `less`
+    case `assign`
+    case `plus`
+    case `minus`
+    case `multiply`
+    case `divide`
+    case `modulo`
+
+    // Punctuation
+    case comma = 4000
+    case dot
+
+    // Delimeters
+    case semicolon = 5000
+    case lparen
+    case rparen
+    case lbrace
+    case rbrace
+    case lbracket
+    case rbracket
+  }
+
+  /// The kind of token.
+  public let kind: Kind
+
+  /// The location from which token was extracted.
+  public let location: SourceRange
+
+  /// Creates an instance of token with given `kind` and `location`.
+  public init(_ kind: Kind, at location: SourceRange) {
+    self.kind = kind
+    self.location = location
+  }
+}
