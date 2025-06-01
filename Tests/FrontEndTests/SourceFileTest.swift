ec90533eaa3686c04bdb29eaf77fc33e8ff74ddf
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2025-06-02 01:05:23 +0530

    more tests for line and col

--- Tests/FrontEndTests/SourceFileTest.swift
+++ Tests/FrontEndTests/SourceFileTest.swift
@@ -16,9 +16,24 @@ import Testing
   }
 
   @Test func lineAndCol() {
-    let idx = str.index(str.startIndex, offsetBy: 11)
-    let (line, col) = source.lineAndCol(of: idx)
+    var idx = str.index(str.startIndex, offsetBy: 11)
+    var (line, col) = source.lineAndCol(of: idx)
     #expect(line == 3)
     #expect(col == 2)
+
+    idx = str.startIndex
+    (line, col) = source.lineAndCol(of: idx)
+    #expect(line == 1)
+    #expect(col == 1)
+
+    idx = str.index(str.startIndex, offsetBy: 6)
+    (line, col) = source.lineAndCol(of: idx)
+    #expect(line == 2)
+    #expect(col == 1)
+
+    idx = str.index(str.endIndex, offsetBy: -1)
+    (line, col) = source.lineAndCol(of: idx)
+    #expect(line == 4)
+    #expect(col == 1)
   }
 }
